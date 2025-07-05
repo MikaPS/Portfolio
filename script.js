fc1624693bb5079e7c4abe3fb6159651fe8bd42e
MikaPS <mikaps7@gmail.com>
2025-07-05 01:42:39 -0700

    Initial Design

---
+++ script.js
@@ -0,0 +1,10 @@
+function showTab(index) {
+  const panels = document.querySelectorAll(".tab-panel");
+  const buttons = document.querySelectorAll(".tab-button");
+
+  panels.forEach((panel) => panel.classList.remove("active"));
+  buttons.forEach((button) => button.classList.remove("active"));
+
+  panels[index].classList.add("active");
+  buttons[index].classList.add("active");
+}
